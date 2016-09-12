56f0ba85d3b2650f479b260f69c44f5299573a5e
u3503025 <ydg19940714@gmail.com>
2016-09-12 11:17:16 -0500

    solve modified, graph plotted, code remains to be cleaned

--- GD.R
+++ GD.R
@@ -19,43 +19,47 @@ m<- matrix(rep(1,569),nrow=length(y));
 
 #set b=1
 
-GD<- function(x,y,m,round=200,method="g"){
+GD<- function(x,y,m,round=200,step1=0.001,step2=0.001){
 	b=matrix(rep(0,dim(x)[2]),nrow=dim(x)[2]);
-	step=0.001;
-	beta=0.8;
-	
-	if(method=="g"){
-		for(r in 1:round){
-		
-			g= gradient(x,y,b,m);
-			b= b-step*g;
-		
-			#print(g);
-			print(nll(x,y,b,m));
+	#beta=0.8;
 
-			#if(negativeL(x,y,b-g,m)> nl- step/2*sum(g*g)){
-			#	step=beta*step;
-			#}
-		}
-		print(b);
-	}
 	
-	else if(method=="n"){
-		for(r in 1:round){
+	p.x<-c();
+	p.y1<-c();
+	p.y2<-c();
+	
+	for(r in 1:round){
 		
-			g= ngradient(x,y,b,m);
-			b= b-step*g;
-		
-			#print(g);
-			print(nll(x,y,b,m));
+		g= gradient(x,y,b,m);
+		b= b-step1*g;
+		p.x<-c(p.x,r);
+		p.y1<-c(p.y1,nll(x,y,b,m));
+		#print(g);
+		#print(nll(x,y,b,m));
 
-			#if(negativeL(x,y,b-g,m)> nl- step/2*sum(g*g)){
-			#	step=beta*step;
-			#}
-		}
-		print(b);
+		#if(negativeL(x,y,b-g,m)> nl- step/2*sum(g*g)){
+		#	step=beta*step;
+		#}
 	}
+	#print(b);
+	plot(p.x,p.y1,col="red",xlab="round",ylab="negative log likelihood",main="green: newton's method, red: gradient descent");
 	
+	b=matrix(rep(0,dim(x)[2]),nrow=dim(x)[2]);
+	for(r in 1:round){
+		
+		g= ngradient(x,y,b,m);
+		b= b-step2*g;
+		p.y2<-c(p.y2,nll(x,y,b,m));
+		#print(g);
+		#print(nll(x,y,b,m));
+
+		#if(negativeL(x,y,b-g,m)> nl- step/2*sum(g*g)){
+		#	step=beta*step;
+		#}
+	}
+	#print(b);
+
+	lines(p.x,p.y2,col="green");
 }
 
 nll<-function(x,y,b,m){
@@ -66,6 +70,7 @@ nll<-function(x,y,b,m){
 	return(-l);
 }
 
+
 gradient<-function(x,y,b,m){
 	g<- t(x) %*% (m/(1+exp(-x %*% b))-y);
 	return(g);
@@ -74,8 +79,8 @@ gradient<-function(x,y,b,m){
 ngradient<-function(x,y,b,m){
 	temp<- exp(-x%*%b);
 	w<- diag(c(m* temp/(1+temp)^2));
-	hessian<- t(x)%*%w%*%x;
-	g<- solve(hessian) %*% gradient(x,y,b,m);
+	hessian<- t(x) %*% w %*% x;
+	g<- solve(hessian,gradient(x,y,b,m));
 	return(g);
 }
 
